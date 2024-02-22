cfc2415f8a672ceed787b5ef8cc9cc76c0b855c6
Mauricio <al221911210@gmail.com>
2024-02-22 09:15:22 -0600

    Introduccion a JavaScript

---
+++ 01-Fundamentos/assets/css/js/alerts.js
@@ -0,0 +1,18 @@
+
+
+//Bloquea el navegador 
+//alert('Hola mundo');
+
+//Retorno lo que el usuario escriba 
+
+//let nombre = prompt(' ¿Cual es tu nombre?  ');
+//console.log( nombre );
+//console.log( '****' + nombre + '****' ); //'string vacio'
+
+
+const seleccion = confirm('¿ESTAS SEGURO DE BORRAR ESTO?');
+console.log(seleccion);
+
+
+
+
